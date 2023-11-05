2f721e44c8835336b852c3f946deb234a26db46f
ArmandoDev <jorge.armando.c.cardenas@gmail.com>
2023-11-05 17:26:45 -0600

    Refactor MainNav component to use useState hook for current page tracking

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,30 +1,47 @@
+import { useState } from 'react'
 import './MainNav.css'
 
 // aria-current='page', esto va en el elemento con la clase active
 export const MainNav = () => {
+  const [currentPage, setCurrentPage] = useState('Projects')
+
   return (
-    <nav id='th-nav' aria-label='Main Navigation' className='th-nav'>
-      <ul className='th-nav-list'>
-        <li className='th-nav-list-item'>
-          <a className='th-nav-list-item-link' href='#projects'>
+    <nav id='nav' aria-label='Main Navigation' className='nav'>
+      <h2 className='nav-current-page'>{currentPage}</h2>
+      <ul className='nav-list'>
+        <li className='nav-list-item'>
+          <a
+            className={currentPage === 'Projects' ? 'nav-list-item-link active' : 'nav-list-item-link'}
+            href='#projects'
+            onClick={() => setCurrentPage('Projects')}
+          >
             Projects
           </a>
         </li>
-        <li className='th-nav-list-item'>
-          <a className='th-nav-list-item-link' href='#stack'>
+        <li className='nav-list-item'>
+          <a
+            className={currentPage === 'Stack' ? 'nav-list-item-link active' : 'nav-list-item-link'}
+            href='#stack'
+            onClick={() => setCurrentPage('Stack')}
+          >
             Stack
           </a>
         </li>
-        <li className='th-nav-list-item'>
+        <li className='nav-list-item'>
           <a
-            className='th-nav-list-item-link'
+            className={currentPage === 'About' ? 'nav-list-item-link active' : 'nav-list-item-link'}
             href='#about'
+            onClick={() => setCurrentPage('About')}
           >
             About
           </a>
         </li>
-        <li className='th-nav-list-item'>
-          <a className='th-nav-list-item-link' href='#contact'>
+        <li className='nav-list-item'>
+          <a
+            className={currentPage === 'Contact' ? 'nav-list-item-link active' : 'nav-list-item-link'}
+            href='#contact'
+            onClick={() => setCurrentPage('Contact')}
+          >
             Contact
           </a>
         </li>
