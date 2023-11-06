cb8f2f569c3ef73cd2b43e257b9cd7aa444f1cca
ArmandoDev <jorge.armando.c.cardenas@gmail.com>
2023-11-05 19:19:12 -0600

    Add stack list to Stack component and import stack.json mock data

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,7 +1,23 @@
+import stackLanguages from './../mocks/stack.json'
+import './css/Stack.css' // Asegúrate de importar tu hoja de estilos
+
 export const Stack = () => {
+  const stackList = stackLanguages.stack
+
   return (
     <section id='stack'>
-      <h1>Stack</h1>
+      <ul className='stack-list' role='list'>
+        {stackList.map((item) => (
+          <li key={item.id} className='stack-list-item' role='listitem'>
+            <img
+              src={item.icon}
+              alt={item.name}
+              className='stack-list-item-image'
+            />
+            <p className='stack-list-item-name'>{item.name}</p>
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
